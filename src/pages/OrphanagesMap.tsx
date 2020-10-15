b1159c825fd09c55a3072666a564f743034413d1
Jefferson Gouveia <jeffersongouveia@eucatur.com.br>
2020-10-15 12:43:58 -0400

    📱 Happy app with orphanages map

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,119 @@
+import React from 'react'
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps'
+import { Feather } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/native'
+
+import mapMarker from '../../assets/images/map-marker.png'
+
+export default function OrphanagesMap() {
+  const navigation = useNavigation()
+
+  function handleNavigateToOrphanageDetails() {
+    navigation.navigate('OrphanageDetails')
+  }
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: -10.8638136,
+          longitude: -61.9696815,
+          latitudeDelta: 0.012,
+          longitudeDelta: 0.012,
+        }}
+      >
+        <Marker
+          icon={mapMarker}
+          coordinate={{
+            latitude: -10.8638136,
+            longitude: -61.9696815,
+          }}
+          calloutAnchor={{
+            x: 0.5,
+            y: 1.9,
+          }}
+        >
+          <Callout tooltip onPress={handleNavigateToOrphanageDetails}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>
+                Mansão Foster
+              </Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          2 orfanatos encontrados
+        </Text>
+
+        <TouchableOpacity style={styles.footerBtnCreateOrphanage}>
+          <Feather name="plus" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: '#FFF',
+    borderRadius: 16,
+    justifyContent: 'center',
+  },
+
+  calloutText: {
+    color: '#0089A5',
+    fontSize: 14,
+    fontFamily: 'Nunito_700Bold',
+  },
+
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+
+    backgroundColor: '#FFF',
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+
+    elevation: 3,
+  },
+
+  footerText: {
+    color: '#8FA7B3',
+    fontFamily: 'Nunito_700Bold',
+  },
+
+  footerBtnCreateOrphanage: {
+    width: 56,
+    height: 56,
+    backgroundColor: '#15C3D6',
+    borderRadius: 20,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
